dc267ab226c95593d8c8e05bab97bd2919402210
MOSTA3D <khabbieny123@gmail.com>
2022-02-26 20:07:27 +0200

    test without setenv

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -2,13 +2,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  port: Number(process.env.PORT),
-  host: process.env.POSTGRES_HOST,
+  username: "postgres",//`${process.env.POSTGRES_USERNAME}`,
+  password: "postgres",//process.env.POSTGRES_PASSWORD,
+  database: "postgres",//process.env.POSTGRES_DB,
+  port: 5432,//Number(process.env.PORT),
+  host: "database-1.cbplw7u4yuyy.us-east-1.rds.amazonaws.com",//process.env.POSTGRES_HOST,
   dialect: "postgres",
-  aws_region: process.env.AWS_REGION,
+  aws_region: "us-east-1",//process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
