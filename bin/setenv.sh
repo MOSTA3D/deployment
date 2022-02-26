ba523d129e615d811d9ba0a075ce22a0c4f191c8
MOSTA3D <khabbieny123@gmail.com>
2022-02-26 22:26:55 +0200

    fixed commit

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,7 +1,1 @@
-# export POSTGRES_USERNAME="postgres" &&
-# export POSTGRES_PASSWORD="postgres" &&
-# export POSTGRES_DB="postgres" &&
-# export POSTGRES_HOST="database-1.cbplw7u4yuyy.us-east-1.rds.amazonaws.com" &&
-# export PORT=5432 &&
-# export AWS_REGION=us-east-1 &&
-# export JWT_SECRET=secret
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION
